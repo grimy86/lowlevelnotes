77c23fe9f00ffc873bb68aa80535c342bf04a061
grimy86 <grimy86@proton.me>
2026-01-01 22:29:37 +0100

    vercel

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ createApp({
       siteName: "lowlevelnotes",
       tagline: "Structured notes on programming, low-level concepts, and system internals.",
 
+       people: [
+        {
+          name: "Grimy86",
+          role: "Author & Maintainer",
+          avatar: "https://avatars.githubusercontent.com/u/105457539?v=4",
+          profile: "https://github.com/grimy86",
+          external: false
+        }
+      ],
+
       resources: [
         {
           title: "C-style C++",
+          author: "Grimy86",
           description: "2024 Edition",
           pdf: "/resources/pdfs/c_style_cpp.pdf"
         },
@@ -48,6 +59,9 @@ createApp({
     openResource(resource) {
       if (!resource.pdf) return;
       window.open(resource.pdf, "_blank");
+    },
+    getAuthor(name) {
+      return this.people.find(p => p.name === name);
     }
   }
 }).mount("#app");
